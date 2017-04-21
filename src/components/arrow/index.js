c2ff6277e03d83cc0086dd88ce7c1cae2fb887c8
Yura Trambitskiy <tr.yura@yahoo.com>
2017-04-22 01:06:28 +0300

    Add 'arrow' component

---
+++ src/components/arrow/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Radium from 'radium';
+
+const Arrow = (props) => {
+  let transform;
+
+  const style = {
+    base: {
+      textAlign: 'center',
+      display: 'inline-block',
+      width: '32px',
+      height: '32px',
+      transform,
+      ...props.style,
+    },
+    top: {
+      transform: 'rotate(0deg)',
+    },
+    right: {
+      transform: 'rotate(90deg)',
+    },
+    bottom: {
+      transform: 'rotate(180deg)',
+    },
+    left: {
+      transform: 'rotate(270deg)',
+    },
+  };
+
+  return (
+    <div style={[style.base, style[props.direction]]} >
+      <svg width="6" height="31" viewBox="0 0 6 31" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2 4v27h2V4H2zM0 4h6L3 0 0 4z" fill="#aaa" fillRule="evenodd" />
+      </svg>
+    </div>
+  );
+};
+
+Arrow.defaultProps = {
+  style: {},
+};
+
+Arrow.propTypes = {
+  direction: PropTypes.oneOf(['top', 'right', 'bottom', 'left']).isRequired,
+  style: PropTypes.object,
+};
+
+export default Radium(Arrow);
--- src/components/slide/index.js
+++ src/components/slide/index.js
@@ -4,24 +4,25 @@ import Radium from 'radium';
 import PropTypes from 'prop-types';
 
 import { mapStateToProps, mapDispatchToProps } from './connector';
+import Arrow from '../arrow';
 
-export class SlideContainer extends React.PureComponent {
-  render() {
-    const { words, index } = this.props;
-
-    return (
-      <div
-        style={{
-          margin: '12px 0',
-          fontFamily: 'Catamaran, sans-serif',
-          fontWeight: 300,
-          fontSize: '36px',
-          textAlign: 'center',
-        }}
-      >{words[index % words.length]}</div>
-    );
-  }
-}
+export const SlideContainer = props => (
+  <div
+    style={{
+      margin: '12px 0',
+      padding: '12px 24px',
+      fontFamily: 'Catamaran, sans-serif',
+      fontWeight: 300,
+      fontSize: '36px',
+      lineHeight: '40px',
+      textAlign: 'center',
+    }}
+  >
+    <Arrow direction="left" style={{ float: 'left' }} />
+    <Arrow direction="right" style={{ float: 'right' }} />
+    {props.words[props.index % props.words.length]}
+  </div>
+);
 
 SlideContainer.defaultProps = {
   words: null,
